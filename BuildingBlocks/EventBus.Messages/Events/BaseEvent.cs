e2fac8674addc387e872eb9e632cbcbbbf0e1d2e
Uthman <uthmanrahimi@gmail.com>
2021-12-06 15:03:25 +0330

    Added baseEvent ,basketCheckoutEvent and EventBusConstants

---
+++ BuildingBlocks/EventBus.Messages/Events/BaseEvent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventBus.Messages.Events
+{
+    public class BaseEvent
+    {
+        public Guid Id { get; init; }
+        public DateTime CreationDate { get; set; }
+    }
+}
